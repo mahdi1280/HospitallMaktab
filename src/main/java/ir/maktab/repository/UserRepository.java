3012556e32faa83a0dce70155fc6fddf3eb7ef5b
mahdi <mahdikarimi422@gmail.com>
2022-04-12 13:51:52 +0430

    test for user repository.

--- src/main/java/ir/maktab/repository/UserRepository.java
+++ src/main/java/ir/maktab/repository/UserRepository.java
@@ -8,59 +8,58 @@ import java.util.List;
 
 public interface UserRepository {
 
-    default void save(User user){
+    default void save(User user) {
         Session instance = MySession.getInstance();
         instance.beginTransaction();
         instance.save(user);
         instance.getTransaction().commit();
     }
 
-    default User findByUsername(String username){
+    default User findByUsername(String username) {
         Session instance = MySession.getInstance();
-        instance.beginTransaction();
-        User user = (User) instance.createQuery("from User where username = :username")
+        List<User> username1 = instance.createQuery("select u from User u where u.username = :username", User.class)
                 .setParameter("username", username)
-                .getSingleResult();
-        instance.getTransaction().commit();
-        return user;
+                .getResultList();
+        if(username1.isEmpty())
+            return null;
+        return username1.get(0);
     }
 
-    default User findById(Long id){
+    default User findById(Long id) {
         Session instance = MySession.getInstance();
-        instance.beginTransaction();
-        User user = (User) instance.createQuery("from User where id = :id")
-                .setParameter("id", id)
-                .getSingleResult();
-        instance.getTransaction().commit();
-        return user;
+        List<User> id1 = instance.createQuery("select u from User u where u.id = :id", User.class)
+                .setParameter("id", id).getResultList();
+        if (id1.isEmpty())
+            return null;
+        return id1.get(0);
     }
 
-    default void update(User user){
+    default void update(User user) {
         Session instance = MySession.getInstance();
         instance.beginTransaction();
         instance.update(user);
         instance.getTransaction().commit();
     }
 
-    default void delete(User user){
+    default void delete(User user) {
         Session instance = MySession.getInstance();
         instance.beginTransaction();
         instance.delete(user);
         instance.getTransaction().commit();
     }
 
-    default User findByUsernameAndPassword(String username, String password){
+    default User findByUsernameAndPassword(String username, String password) {
         Session instance = MySession.getInstance();
         List<User> resultList = instance.createQuery("select u from User u where u.username=:username and u.password=:password", User.class)
                 .setParameter("username", username)
                 .setParameter("password", password)
                 .getResultList();
-        if(resultList.size() == 0)
+        if (resultList.size() == 0)
             return null;
         return resultList.get(0);
     }
 
-    default List<User> showAll(){
+    default List<User> showAll() {
         Session instance = MySession.getInstance();
         instance.beginTransaction();
         List<User> users = instance.createQuery("from User").getResultList();
--- src/test/java/ir/maktab/repository/UserRepositoryTest.java
+++ src/test/java/ir/maktab/repository/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package ir.maktab.repository;
 
 import ir.maktab.model.Role;
 import ir.maktab.model.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class UserRepositoryTest {
@@ -21,25 +22,35 @@ class UserRepositoryTest {
 
     @Test
     void findByUsername() {
+        User ali = userRepository.findByUsername("daqewdsfdsfertertali");
+        Assertions.assertNull(ali);
     }
 
     @Test
     void findById() {
+        User byId = userRepository.findById(1000L);
+        Assertions.assertNull(byId);
     }
 
     @Test
     void update() {
+        User user = User.builder()
+                .password("123")
+                .username("ali")
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        userRepository.update(user);
     }
 
     @Test
     void delete() {
+        userRepository.delete(new User());
     }
 
     @Test
     void findByUsernameAndPassword() {
-    }
-
-    @Test
-    void showAll() {
+        User ali = userRepository.findByUsernameAndPassword("erwerw", "wer234234234");
+        Assertions.assertNull(ali);
     }
 }
